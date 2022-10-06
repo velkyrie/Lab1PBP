5a30b20118b907f5bbbfb14c8646a283ff736aba
jinggamentari <vladijm@gmail.com>
2022-10-06 16:37:48 +0700

    AJAX GET done

--- wishlist/views.py
+++ wishlist/views.py
@@ -25,6 +25,16 @@ def show_wishlist(request):
     }
     return render(request, "wishlist.html", context)
 
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Vladi Jingga Mentari',
+        'last_login': request.COOKIES['last_login'],
+        
+    }
+    return render(request, "wishlist_ajax.html", context)
+
 def show_xml(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
